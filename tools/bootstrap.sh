33c72294069a8ea8cfc9dfd18e6c93b0a469a305
freemountain <dodo@dodoMBP.local>
2016-11-21 14:41:54 +0100

    build patchelf

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -2,7 +2,9 @@
 set -e
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
-OS="$($PROJECT_PATH/tools/arch.sh -f node -o)"
+OS="$($PROJECT_PATH/tools/arch.sh -o)"
+ARCH="$($PROJECT_PATH/tools/arch.sh -a)"
+
 echo "download node..."
 NODE_CMD=$("$PROJECT_PATH/tools/downloadNode.sh")
 NPM_CMD="$(dirname "$NODE_CMD")/npm"
@@ -23,20 +25,39 @@ cd "$PROJECT_PATH/src/node_path"
 "$NODE_CMD" "$NPM_CMD" install
 
 if [ "$OS" = "linux" ]; then
-    if [ ! -d "$PROJECT_PATH/tmp/linuxdeployqt-src" ] ; then
-        git clone "https://github.com/probonopd/linuxdeployqt" "$PROJECT_PATH/tmp/linuxdeployqt-src"
-    fi
-    if [ ! -d "$PROJECT_PATH/tmp/linuxdeployqt-build" ] ; then
-        mkdir "$PROJECT_PATH/tmp/linuxdeployqt-build"
-        cd "$PROJECT_PATH/tmp/linuxdeployqt-build"
-        qmake "../linuxdeployqt-src"
-        make
-    fi
+  BIN_PATH="$PROJECT_PATH/tmp/bin-$OS-$ARCH"
+  mkdir -p "$BIN_PATH"
 
-    APPIMG_CMD="$PROJECT_PATH/tmp/appimagetool"
-    if [ ! -f "$APPIMG_CMD" ] ; then
-        curl -L -o "$APPIMG_CMD" "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
-        chmod +x "$APPIMG_CMD"
-    fi
+  if [ ! -d "$PROJECT_PATH/tmp/linuxdeployqt-src" ] ; then
+      git clone "https://github.com/probonopd/linuxdeployqt" "$PROJECT_PATH/tmp/linuxdeployqt-src"
+  fi
+
+  if [ ! -d "$PROJECT_PATH/tmp/patchelf-src" ] ; then
+      git clone "https://github.com/NixOS/patchelf" "$PROJECT_PATH/tmp/patchelf-src"
+      ./bootstrap.sh
+  fi
+
+  if [ ! -d "$PROJECT_PATH/tmp/linuxdeployqt-build" ] ; then
+      mkdir "$PROJECT_PATH/tmp/linuxdeployqt-build"
+      cd "$PROJECT_PATH/tmp/linuxdeployqt-build"
+      qmake "../linuxdeployqt-src"
+      make
+      cp "linuxdeployqt/linuxdeployqt" "$BIN_PATH/linuxdeployqt"
+  fi
+
+  if [ ! -d "$PROJECT_PATH/tmp/patchelf-build" ] ; then
+      mkdir "$PROJECT_PATH/tmp/patchelf-build"
+      cd "$PROJECT_PATH/tmp/patchelf-build"
+      "$PROJECT_PATH/tmp/patchelf-src/configure" --prefix="$PROJECT_PATH/tmp/patchelf-build"
+      make
+      make install
+      cp "$PROJECT_PATH/tmp/patchelf-build/bin/patchelf" "$BIN_PATH/patchelf"
+  fi
+
+  APPIMG_CMD="$BIN_PATH/appimagetool"
+  if [ ! -f "$APPIMG_CMD" ] ; then
+      curl -L -o "$APPIMG_CMD" "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+      chmod +x "$APPIMG_CMD"
+  fi
 fi
 popd > /dev/null
--- tools/deploy_linux.sh
+++ tools/deploy_linux.sh
@@ -4,7 +4,9 @@ PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
 TARGET_APP=$1
 TARGET_PATH=$(dirname "$TARGET_APP")
 DEPLOY_CMD="$PROJECT_PATH/tmp/linuxdeployqt-build/linuxdeployqt/linuxdeployqt"
-TMP_PATH="$PROJECT_PATH/tmp"
+OS="$($PROJECT_PATH/tools/arch.sh -o)"
+ARCH="$($PROJECT_PATH/tools/arch.sh -a)"
+BIN_PATH="$PROJECT_PATH/tmp/bin-$OS-$ARCH"
 
 cat << EOF > "$TARGET_PATH/quark.desktop"
 [Desktop Entry]
@@ -18,7 +20,7 @@ EOF
 
 cp "$PROJECT_PATH/quark.svg" "$TARGET_PATH/default.svg"
 
-PATH="$TMP_PATH:$PATH" "$DEPLOY_CMD" $TARGET_APP -qmldir=$PROJECT_PATH/src/qml -bundle-non-qt-libs -no-strip
+PATH="$BIN_PATH:$PATH" "$DEPLOY_CMD" $TARGET_APP -qmldir=$PROJECT_PATH/src/qml -bundle-non-qt-libs -no-strip
 
 # we need to run this two times...
-PATH="$TMP_PATH:$PATH" "$DEPLOY_CMD" $TARGET_APP -appimage -qmldir=$PROJECT_PATH/src/qml -bundle-non-qt-libs -no-strip
+PATH="$BIN_PATH:$PATH" "$DEPLOY_CMD" $TARGET_APP -appimage -qmldir=$PROJECT_PATH/src/qml -bundle-non-qt-libs -no-strip
