db393a93ab368f61fd534e38ef015a1565af8700
freemountain <dodo@dodoMBP.local>
2016-11-21 19:43:05 +0100

    change ci builds n7

--- tools/travis.sh
+++ tools/travis.sh
@@ -14,8 +14,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   cat /opt/qt57/bin/qt57-env.sh
   #source /opt/qt57/bin/qt57-env.sh
   export PATH=/opt/qt57/bin:$PATH
-  tools/bootstrap.sh
-  tools/bootstrap.sh
+  tools/bootstrap.sh || tools/bootstrap.sh
   mkdir build && cd build && qmake .. && make && make clean && cd ..
   tools/deploy_linux.sh build/quark
   mv Quark-x86_64.AppImage build/deploy/Quark-x86_64.AppImage
