c3187f264d35d700a5d08cafa8153c33ad840830
freemountain <dodo@dodoMBP.local>
2016-11-21 17:00:22 +0100

    change ci builds

--- tools/downloadQpm.sh
+++ tools/downloadQpm.sh
@@ -14,6 +14,13 @@ mkdir -p "$BASE_PATH"
 URL="$BASE_URL/$OS""_$ARCH/qpm"
 CMD="$BASE_PATH/qpm"
 
+case "$OS" in
+  darwin) URL="$BASE_URL/darwin_386/qpm";;
+  linux) URL="$BASE_URL/linux_386/qpm";;
+  windows) URL="$BASE_URL/windows_386/qpm.exe" CMD="$BASE_PATH/qpm.exe" ;;
+  *)  echo "could not parse uname -m output: $arcg" ; exit 1;
+esac
+
 if [ -f "$CMD" ]; then
   echo "$CMD"
   exit
