d947803c7af7de15c66a36ff8cf5076ebfe8ae36
Dmnk Freemountainer <Dmnk Freemountainer>
2016-11-24 13:05:11 +0100

    trigger ci 13

--- tools/deploy_win.sh
+++ tools/deploy_win.sh
@@ -4,4 +4,4 @@ PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
 TARGET_APP=$1
 TARGET_PATH=$(dirname "$TARGET_APP")
 
-windeployqt $TARGET_APP  --compiler-runtime --qmldir=$PROJECT_PATH/src/qml
+windeployqt $TARGET_APP  --compiler-runtime --release --qmldir=$PROJECT_PATH/src/qml
