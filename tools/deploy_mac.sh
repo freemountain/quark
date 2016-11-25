705b9312b9bed82f3625c8eca747db5ff36dc127
freemountain <dodo@dodoMBP.local>
2016-11-25 14:02:18 +0100

    Revert "prune node_modules on deploy"
    This reverts commit 0368c5cc0bfad71634c2c5e11ac493ec4719c385.

--- tools/deploy_mac.sh
+++ tools/deploy_mac.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
-TARGET_APP=$(realpath $1)
-
-OS="$($PROJECT_PATH/tools/uname.sh -o)"
-ARCH="$($PROJECT_PATH/tools/uname.sh -a)"
-BIN_PATH="$PROJECT_PATH/tmp/bin-$OS-$ARCH"
-
-pushd . >> /dev/null
-cd "$TARGET_APP/Contents/Resources/node_path"
-"$BIN_PATH/node" "$BIN_PATH/npm" prune --production
-cd "$TARGET_APP/Contents/Resources/default"
-"$BIN_PATH/node" "$BIN_PATH/npm" prune --production
-popd >> /dev/null
+TARGET_APP=$1
 
 macdeployqt $TARGET_APP -qmldir=$PROJECT_PATH/src/qml -no-strip
