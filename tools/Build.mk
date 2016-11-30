a3195cf520a4bd41374fbaa7efad124415f0a929
freemountain <dodo@dodoMBP.local>
2016-11-30 21:15:30 +0100

    not working

--- tools/Build.mk
+++ tools/Build.mk
@@ -1,10 +1,10 @@
 ##
 #  default build (only osx)
 #
-BUILD_DIR:=$(PROJECT_PATH)/build
 JS_SRC:=$(PROJECT_PATH)/src/node_path
+JS_BUILD:=$(TMP_PATH)/node_path
 JS_OBJECTS:= \
-	$(foreach x, $(shell cd $(JS_SRC)/src && find . -type f -iname '*.js'), $(TMP_PATH)/node_path/lib/$(x)) \
+	$(foreach x, $(shell cd $(JS_SRC)/src && find . -type f -iname '*.js'), $(JS_BUILD)/lib/$(x)) \
 
 ################################
 ########## Build C++ ###########
@@ -13,43 +13,32 @@ JS_OBJECTS:= \
 ##
 #  copy entrypoint to TMP_PATH
 #
-$(TMP_PATH)/node_path/quark.js:
+$(JS_BUILD)/quark.js:
 	mkdir -p $(dir $@)
 	cp  $(JS_SRC)/quark.js $@
 
 ##
 #  copy package.json to TMP_PATH
 #
-$(TMP_PATH)/node_path/package.json:
+$(JS_BUILD)/package.json:
 	mkdir -p $(dir $@)
 	cp  $(JS_SRC)/package.json $@
 
 ##
 #  install node modules in tmp
 #
-$(TMP_PATH)/node_path/node_modules: $(TMP_PATH)/node_path/package.json $(TOOLS)
-	cd $(TMP_PATH)/node_path; $(NPM_CMD) install
+$(JS_BUILD)/node_modules: $(JS_BUILD)/package.json $(TOOLS)
+	cd $(JS_BUILD); $(NPM_CMD) install
 
-##
-#  TODO: drop dependency js-build, by copying
-#  the relevant resources here, instead of in
-#  qmake. this should fix the problems with
-#  parallel build too
-#
-#  builds the qt/c++ stuff
-#
-$(BUILD_DIR)/quark.app: $(TMP_PATH)/node_path/node_modules $(TMP_PATH)/node_path/quark.js js-build qpm-install
-	mkdir -p $(BUILD_DIR)
-	mkdir -p $@
-	mkdir -p $@/Contents
-	mkdir -p $@/Contents/Resources
-	mkdir -p $@/Contents/MacOS
-	cd $(BUILD_DIR) && qmake ..
-	cd $(BUILD_DIR) && make
-
-################################
+#########################
 ################################
 
+################################
+########### Build CPP ##########
+################################
+
+cpp-build: js-build
+	$(MAKE) --makefile qmake.mk
 
 ################################
 ########### Build JS ###########
@@ -58,16 +47,16 @@ $(BUILD_DIR)/quark.app: $(TMP_PATH)/node_path/node_modules $(TMP_PATH)/node_path
 ##
 #  js: lint, transpile and move to destination
 #
-$(TMP_PATH)/node_path/lib/%.js: $(JS_SRC)/src/%.js $(TOOLS) $(NPM_PKGS)
+$(JS_BUILD)/lib/%.js: $(JS_SRC)/src/%.js $(TOOLS) $(NPM_PKGS)
 	mkdir -p $(dir $@)
-	$(NODE_CMD) $(JS_SRC)/node_modules/eslint/bin/eslint.js $<
-	$(NODE_CMD) $(JS_SRC)/node_modules/babel-cli/bin/babel.js $< --out-file $@ --source-maps --presets es2017,es2016,node6 --plugins transform-runtime,transform-class-properties
+	$(NODE_CMD) $(JS_BUILD)/node_modules/eslint/bin/eslint.js $<
+	$(NODE_CMD) $(JS_BUILD)/node_modules/babel-cli/bin/babel.js $< --out-file $@ --source-maps --presets es2017,es2016,node6 --plugins transform-runtime,transform-class-properties
 
 ##
 #  unit test js
 #
 js-test: $(JS_OBJECTS)
-	PATH=$(BIN_PATH):$$PATH  $(JS_SRC)/node_modules/.bin/istanbul cover --root $(TMP_PATH)/node_path -x "**/__tests__/**" $(PROJECT_PATH)/src/node_path/node_modules/.bin/_mocha $(shell find $(TMP_PATH)/node_path -name "*Test.js" -not -path "*node_modules*") -- -R spec --require source-map-support/register
+	PATH=$(BIN_PATH):$$PATH $(JS_BUILD)/node_modules/.bin/istanbul cover --root $(JS_BUILD) -x "**/__tests__/**" $(JS_BUILD)/node_path/node_modules/.bin/_mocha $(shell find $(JS_BUILD) -name "*Test.js" -not -path "*node_modules*") -- -R spec --require source-map-support/register
 
 js-build: js-test
 
