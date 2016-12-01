8e458bb3e90eb47bbe203101c1eb9c63cb8454d4
freemountain <dodo@dodoMBP.local>
2016-12-01 19:08:34 +0100

    fix npm install

--- tools/Dependencies.mk
+++ tools/Dependencies.mk
@@ -1,12 +1,11 @@
-PKGS_DEFAULT:=$(addsuffix node_modules, $(wildcard $(PROJECT_PATH)/example/*/))
-PKGS_DEFAULT+=$(PROJECT_PATH)/src/node_path/node_modules
-
-NPM_PKGS:=$(PKGS_DEFAULT) $(TMP_PATH)/node_path/node_modules
+NODE_MODULES:=$(addsuffix node_modules, $(wildcard $(PROJECT_PATH)/example/*/))
+NODE_MODULES+=$(PROJECT_PATH)/src/node_path/node_modules
+NODE_MODULES+=$(TMP_PATH)/node_path/node_modules
 
 ##
 #  install qpm deps
 #
-$(PROJECT_PATH)/vendor: $(TOOLS)
+$(PROJECT_PATH)/vendor: $(TOOLS) $(PROJECT_PATH)/qpm.json
 	cd $(PROJECT_PATH) && $(QPM_CMD) install
 
 ##
@@ -15,31 +14,32 @@ $(PROJECT_PATH)/vendor: $(TOOLS)
 qpm-install: $(PROJECT_PATH)/vendor
 
 ##
-#  install all npm deps
+#  install npm pkgs
 #
-$(PKGS_DEFAULT): $(TOOLS)
+$(PROJECT_PATH)/src/node_path/node_modules: $(TOOLS) $(PROJECT_PATH)/src/node_path/package.json
+	cd $(dir $@) && $(NPM_CMD) install
+
+$(TMP_PATH)/node_path/node_modules: $(TOOLS) $(TMP_PATH)/node_path/package.json
+	cd $(dir $@) && $(NPM_CMD) install
+
+$(PROJECT_PATH)/example/%/node_modules: $(TOOLS) $(PROJECT_PATH)/example/%/package.json
 	cd $(dir $@) && $(NPM_CMD) install
 
 ##
 #  copy entrypoint to TMP_PATH
 #
-$(TMP_PATH)/node_path/quark.js:
+$(TMP_PATH)/node_path/quark.js: $(JS_SRC)/quark.js
 	mkdir -p $(dir $@)
-	cp  $(JS_SRC)/quark.js $@
+	cp $< $@
 
 ##
 #  copy package.json to TMP_PATH
 #
-$(TMP_PATH)/node_path/package.json:
+$(TMP_PATH)/node_path/package.json: $(JS_SRC)/package.json
 	mkdir -p $(dir $@)
-	cp  $(JS_SRC)/package.json $@
-##
-#  install node modules in tmp
-#
-$(PROJECT_PATH)/tmp/node_path/node_modules: $(TMP_PATH)/node_path/package.json $(TOOLS)
-	cd $(dir $@); $(NPM_CMD) install
+	cp $< $@
 
 ##
 #  shortcut
 #
-npm-install: $(NPM_PKGS)
+npm-install: $(NODE_MODULES)
