c243e9807f16d381420857e140d04c32df4c3907
Dmnk Freemountainer <Dmnk Freemountainer>
2016-12-02 12:26:32 +0100

    trigger ci

---
+++ tools/appveyor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir build
+cd build
+./../configure --qt=/c/Qt/5.7/mingw53_32/bin
+mingw32-make -j4
+mingw32-make clean-build
+mingw32-make package
+mv release quark
+7z a quark-windows-x86.zip quark
+ls
