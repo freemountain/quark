0e2258ef5445559ad9589b9e8d3ef7a92775c971
Dmnk Freemountainer <Dmnk Freemountainer>
2016-12-06 11:31:06 +0100

    try to use cygwin2

--- tools/appveyor.sh
+++ tools/appveyor.sh
@@ -2,7 +2,7 @@
 set -e
 export PATH=/c/cygwin/bin:/c/Qt/Tools/mingw53_32/bin:$PATH
 
-bash ./configure --qt=/c/Qt/5.7/mingw53_32
+./configure --qt=/c/Qt/5.7/mingw53_32
 mingw32-make -j4
 mingw32-make clean-build
 mingw32-make package
