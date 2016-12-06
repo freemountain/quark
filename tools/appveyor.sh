b9368c2ac6b57bc934d81f12d2b7fd8134ffcb79
Dmnk Freemountainer <Dmnk Freemountainer>
2016-12-06 11:23:32 +0100

    try to use bash from mysys2

--- tools/appveyor.sh
+++ tools/appveyor.sh
@@ -3,8 +3,7 @@ set -e
 export PATH=/c/mysys64/mingw64/bin:/c/mysys64/usr/bin:$PATH
 export PATH=/c/Qt/Tools/mingw53_32/bin:$PATH
 
-find --help
-./configure --qt=/c/Qt/5.7/mingw53_32
+/c/mysys64/usr/bin/bash ./configure --qt=/c/Qt/5.7/mingw53_32
 mingw32-make -j4
 mingw32-make clean-build
 mingw32-make package
