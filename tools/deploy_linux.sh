7fb9cca2ce6604efc7b95c6f6fee49f1d3620fff
freemountain <you@example.com>
2017-02-03 20:50:16 +0100

    fix linux ci

--- tools/deploy_linux.sh
+++ tools/deploy_linux.sh
@@ -3,14 +3,27 @@
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
 
 TARGET_APP=$1
-TARGET_BIN=$PROJECT_PATH/dist/$TARGET_APP/$TARGET_APP
+TARGET_PATH="$PROJECT_PATH/dist/$TARGET_APP"
+TARGET_BIN="$TARGET_PATH/$TARGET_APP"
 
 QT_PATH="$($PROJECT_PATH/tools/get_qt_path.sh)"
 echo "QT: $QT_PATH"
-DEPLOY_CMD="./build/tools/linuxdeployqt $TARGET_BIN -verbose=2 -qmldir=$PROJECT_PATH/src/libquark/qml"
+DEPLOY_CMD="./build/tools/linuxdeployqt $TARGET_BIN -no-strip -verbose=2 -qmldir=$PROJECT_PATH/src/libquark/qml"
 
 "$PROJECT_PATH/qbs_wrapper" install --install-root $PROJECT_PATH/dist -p $TARGET_APP
+echo "HELLOOO $TARGET_PATH"
+cat << EOF > "$TARGET_PATH/quark.desktop"
+[Desktop Entry]
+Type=Application
+Name=Quark
+Exec=AppRun %F
+Icon=quark
+Comment=Edit this default file
+Terminal=true
+EOF
+
+cp quark.svg dist/$TARGET_APP/quark.svg
 
 PATH="$QT_PATH:$PATH"
 $DEPLOY_CMD
-$DEPLOY_CMD
+$DEPLOY_CMD -appimage
