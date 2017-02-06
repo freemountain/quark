d5895c1fb1391c6a673bf8f8c89b5f09d1b04b61
freemountain <dodo@dodoMBP.local>
2017-02-06 23:14:54 +0100

    change argument handling

--- src/quark-gui/main.cpp
+++ src/quark-gui/main.cpp
@@ -1,30 +1,30 @@
-#include <QQmlApplicationEngine>
 #include <QGuiApplication>
-#include <QtGui/QGuiApplication>
 #include <QObject>
+#include <QQmlApplicationEngine>
 #include <QtDebug>
+#include <QtGui/QGuiApplication>
 
-#include "quarkprocess.h"
 #include "environment.h"
+#include "quarkprocess.h"
 
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
+int main(int argc, char* argv[]) {
+  QGuiApplication app(argc, argv);
 
-    Environment* env = new Environment(app.arguments());
-    QuarkProcess* proc;
+  Environment* env = new Environment(app.arguments());
+  QuarkProcess* proc;
 
-    env->printLine("node:" + env->getCommand("node"));
-    env->printLine("NODE_PATH" + env->getProcEnv().value("NODE_PATH"));
-    env->printLine("script:" + env->getScriptPath());
-    env->printLine("data:" + env->getDataPath().path());
-    env->printLine("bundled app:" + env->getBundledAppPath());
+  env->printLine("node:" + env->getCommand("node"));
+  env->printLine("NODE_PATH" + env->getProcEnv().value("NODE_PATH"));
+  env->printLine("script:" + env->getScriptPath());
+  env->printLine("data:" + env->getDataPath().path());
+  env->printLine("bundled app:" + env->getBundledAppPath());
 
-    if(env->getScriptPath() == "") {
-        proc = env->startProcess(env->getBundledAppPath());
-    } else {
-        proc = env->startProcess(env->getScriptPath());
-    }
+  qDebug() << app.arguments();
+  if (env->getScriptPath() == "") {
+    proc = env->startProcess(env->getBundledAppPath());
+  } else {
+    proc = env->startProcess(env->getScriptPath());
+  }
 
-    return app.exec();
+  return app.exec();
 }
